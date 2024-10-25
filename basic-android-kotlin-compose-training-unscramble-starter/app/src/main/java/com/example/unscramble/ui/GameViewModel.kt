769a0af4249ce33bd35f640b0eabb61c7c7c463a
Léo Pelazza <leo.pelazza@laposte.net>
2024-10-25 02:22:21 -0400

    Fin exercice

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -57,13 +58,24 @@ class GameViewModel : ViewModel() {
     }
 
     private fun updateGameState(updatedScore: Int){
-        _uiState.update { currentState ->
-            currentState.copy(
-                scramblesWord = pickRandomWordAndShuffle(),
-                isGuessedWordWrong = false,
-                score = updatedScore,
-                currentWordCount = currentState.currentWordCount.inc()
-            )
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        }
+        else{
+            _uiState.update { currentState ->
+                currentState.copy(
+                    scramblesWord = pickRandomWordAndShuffle(),
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc()
+                )
+            }
         }
     }
 
@@ -72,7 +84,7 @@ class GameViewModel : ViewModel() {
         updateUserGuess("")
     }
 
-    private fun resetGame(){
+    fun resetGame(){
         usedWords.clear()
         _uiState.value = GameUiState(pickRandomWordAndShuffle())
     }
